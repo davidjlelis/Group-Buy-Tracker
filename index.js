f1e3b1ee440a0d1ea0e9a03755cc1bddf5719494
David Lelis <davidjlelis@gmail.com>
2023-08-20 13:24:16 -0400

    starting the html and initial scraping

--- index.js
+++ index.js
@@ -7,9 +7,15 @@ const main = async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
+
+    const f = await page.$("[class='tab']")
+
+    const text = await (await f.getProperty('textContent')).jsonValue()
+    console.log("Text is: " + text)
+/*
     await page.screenshot({ path: 'screenshot.png'});
     await browser.close();
-
+*/
 }
 
 main();
