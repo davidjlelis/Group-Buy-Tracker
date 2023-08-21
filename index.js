6a1c8db69b8989cafb80765ef71f42017d2d1e96
David Lelis <davidjlelis@gmail.com>
2023-08-21 18:34:26 -0400

    cleaning text 8/21/2023 pt 2

--- index.js
+++ index.js
@@ -8,11 +8,18 @@ const main = async () => {
     const page = await browser.newPage();
     await page.goto(url);
 
-    const f = await page.$("[class='tab']")
+    const f = await page.$("[class='tab']");
 
-    const text = await (await f.getProperty('textContent')).jsonValue()
-    const clean_text = await (await text.replaceAll(/\s/g,''))
-    console.log("Text is: " + clean_text)
+    const text = await (await f.getProperty('textContent')).jsonValue();
+    
+    // remove new line
+    let clean_text = await (await text.replaceAll(/\n/g,''));
+    // separating group buys
+    clean_text = await (await text.replaceAll(/\t/g,'splitting group buys'));
+
+    //clean_text = await (await text.replaceAll(/\v/g,'splitting test'));
+    //console.log("Text is: " + text);
+    console.log("Text is: " + clean_text);
     await browser.close();
 
 
