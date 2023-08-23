5239b429bf991b90003cd80ad6c20999c53016fd
David Lelis <davidjlelis@gmail.com>
2023-08-23 11:30:47 -0400

    Progress Bar cannot be read, possibly because it is dynamically set. Resorted to using Current Status and working off that. Also included Estimated Arrival and the Current Quarter to compare how far along the Group Buy is its process.

--- index.js
+++ index.js
@@ -1,56 +1,85 @@
 import puppeteer from "puppeteer";
 
 async function scrapeGroupBuys() {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    const currentQuarter = getCurrentQuarterAndYear;
 
-  await page.goto('https://novelkeys.com/pages/product-updates');
-  await page.waitForSelector('.tab-button-text', { timeout: 10000 });
+    await page.goto('https://novelkeys.com/pages/product-updates');
+    await page.waitForSelector('.tab-button-text', { timeout: 10000 });
 
-  const tabButtons = await page.$$('.tab-button');
+    const tabButtons = await page.$$('.tab-button');
 
-  const allGroupBuyTypes = [];
+    const allGroupBuyTypes = [];
 
-  for (const tabButton of tabButtons) {
-    const groupBuyType = await tabButton.$eval('.tab-button-text', node => node.textContent.trim());
-    allGroupBuyTypes.push(groupBuyType);
-  }
-
-  const tabs = await page.$$('.tab');
-
-  const allGroupBuys = [];
-
-  for (const tab of tabs) {
-    const groupBuys = [];
-
-    const groupBuyType = allGroupBuyTypes.shift(); // Get the type from the array
-
-    const groupBuyElements = await tab.$$('.preorder-timeline-title');
-
-    for (const element of groupBuyElements) {
-      const title = await element.evaluate(node => node.textContent.trim());
- 
-      // Wait for the <progress> element to become available
-      await page.waitForSelector('.preorder-timeline-progress-bar'); // Replace with your selector
-  
-      // Extract the progress bar text and parse out progress step, progress value, and estimated arrival
-      const progressValue = await page.evaluate(() => {
-        const progressBar = document.querySelector('.preorder-timeline-progress-bar'); // Replace with your selector
-        return progressBar.value;
-      });
-      console.log(progressValue);
-
-      //groupBuys.push({ type: groupBuyType, title, progressStep, progressValue, estimatedArrival });
+    for (const tabButton of tabButtons) {
+        const groupBuyType = await tabButton.$eval('.tab-button-text', node => node.textContent.trim());
+        allGroupBuyTypes.push(groupBuyType);
     }
 
-    //allGroupBuys.push(groupBuys);
-  }
+    const tabs = await page.$$('.tab');
 
-  await browser.close();
+    const allGroupBuys = [];
 
-  return allGroupBuys;
+    for (const tab of tabs) {
+        const groupBuyType = allGroupBuyTypes.shift(); // Get the type from the array
+        const groupBuyElements = await tab.$$('.preorder-timeline-link');
+        const groupBuys = [];
+        for (const element of groupBuyElements) {
+            const title = await element.$eval('.preorder-timeline-title', node => node.textContent.trim());
+
+            // Extract all text content within the .preorder-timeline-details element
+            const detailsText = await element.$eval('.preorder-timeline-details', node => node.textContent);
+
+            // Parse the details text to find "Current Status" and "Estimated Arrival"
+            const currentStatusMatch = detailsText.match(/Current Status:(.*?)Estimated Arrival:/s);
+            const estimatedArrivalMatch = detailsText.match(/Estimated Arrival:(.*?)\n/s);
+
+            // Extract Current Status and Estimated Arrival if found, otherwise set to empty strings
+            const currentStatus = currentStatusMatch ? currentStatusMatch[1].trim() : '';
+            const estimatedArrival = estimatedArrivalMatch ? estimatedArrivalMatch[1].trim() : '';
+
+            //console.log(groupBuyType, title, currentStatus, estimatedArrival);
+            
+            if(currentQuarter !== estimatedArrival){
+                console.log("Not Match");
+            } else {
+                console.log("Match");
+            }
+
+            // Push the data into your groupBuys array if needed
+            groupBuys.push({ type: groupBuyType, title, currentStatus, estimatedArrival });
+        }
+
+        // Push the groupBuys array into the allGroupBuys array if needed
+        allGroupBuys.push(groupBuys);
+    }
+
+    await browser.close();
+
+    return allGroupBuys;
+}
+
+function getCurrentQuarterAndYear() {
+    const now = new Date();
+    const month = now.getMonth() + 1; // JavaScript months are 0-based, so we add 1
+
+    let quarter;
+    if (month >= 1 && month <= 3) {
+        quarter = "Q1";
+    } else if (month >= 4 && month <= 6) {
+        quarter = "Q2";
+    } else if (month >= 7 && month <= 9) {
+        quarter = "Q3";
+    } else {
+        quarter = "Q4";
+    }
+
+    const year = now.getFullYear();
+
+    return (quarter+" "+year);
 }
 
 scrapeGroupBuys().then(data => {
-  console.log(data);
+    console.log(data);
 });
